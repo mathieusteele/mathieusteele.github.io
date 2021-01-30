3a0b659c2569300f3bfdae31b18d48210d0c085e
Mathieu Steele <matt@steeleagency.com>
2021-01-30 12:18:39 -0500

    add many styles

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,42 @@
 function toggleMenu() {
   document.getElementById("hamburger-menu").classList.toggle("hide");
 }
+
+var today = new Date();
+
+document.getElementById("dateToday").innerText = today.toLocaleDateString(
+  "en-GB",
+  {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+);
+
+var dayThree = new Date();
+dayThree.setDate(today.getDate() + 2);
+document.getElementById("day-three").innerText = dayThree.toLocaleDateString(
+  "en-US",
+  {
+    weekday: "long",
+  }
+);
+
+var dayFour = new Date();
+dayFour.setDate(today.getDate() + 3);
+document.getElementById("day-four").innerText = dayFour.toLocaleDateString(
+  "en-US",
+  {
+    weekday: "long",
+  }
+);
+
+var dayFive = new Date();
+dayFive.setDate(today.getDate() + 4);
+document.getElementById("day-five").innerText = dayFive.toLocaleDateString(
+  "en-US",
+  {
+    weekday: "long",
+  }
+);
