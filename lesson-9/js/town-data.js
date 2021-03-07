c9555301d7ea7c15a9d977a5c30e0a1b1e854f88
Mathieu Steele <matt@steeleagency.com>
2021-03-06 21:59:24 -0500

    fix cards

--- lesson-9/js/town-data.js
+++ lesson-9/js/town-data.js
@@ -13,15 +13,15 @@ fetch(requestURL)
       if (townsArray.includes(towns[i].name)) {
         let card = document.createElement("section");
         let deets = document.createElement("div");
-        let h2 = document.createElement("h2");
+        let h3 = document.createElement("h3");
         let motto = document.createElement("p");
         let yearFounded = document.createElement("p");
         let population = document.createElement("p");
         let rainfall = document.createElement("p");
         let photo = document.createElement("img");
 
-        h2.textContent = towns[i].name;
-        motto.textContent = `Motto: ${towns[i].motto}`;
+        h3.textContent = towns[i].name;
+        motto.textContent = `${towns[i].motto}`;
         yearFounded.textContent = `Year founded: ${towns[i].yearFounded}`;
         population.textContent = `Population: ${towns[i].currentPopulation}`;
         rainfall.textContent = `Annual Rain Fall: ${towns[i].averageRainfall}`;
@@ -32,19 +32,13 @@ fetch(requestURL)
           `${towns[i].name} ${towns[i].lastname} - ${towns[i].order}`
         );
 
-        deets.appendChild(h2);
+        deets.appendChild(h3);
         deets.appendChild(motto);
         deets.appendChild(yearFounded);
         deets.appendChild(population);
         deets.appendChild(rainfall);
-
-        // if (i % 2 === 0) {
-        //   card.appendChild(photo);
-        //   card.appendChild(deets);
-        // } else {
         card.appendChild(deets);
         card.appendChild(photo);
-        // }
 
         document.querySelector("div.cards").appendChild(card);
       }
