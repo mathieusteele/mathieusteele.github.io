69498ef893a02f591a8cbaa590342faaf0045118
Mathieu Steele <matt@steeleagency.com>
2021-03-19 22:51:26 -0400

    hide pancake banner on other pages

--- lesson-11/js/script.js
+++ lesson-11/js/script.js
@@ -14,37 +14,18 @@ document.getElementById("dateToday").innerText = today.toLocaleDateString(
   }
 );
 
-// var dayThree = new Date();
-// dayThree.setDate(today.getDate() + 2);
-// document.querySelector("#day-three").innerText = dayThree.toLocaleDateString(
-//   "en-US",
-//   {
-//     weekday: "long",
-//   }
-// );
-
-// var dayFour = new Date();
-// dayFour.setDate(today.getDate() + 3);
-// document.querySelector("#day-four").innerText = dayFour.toLocaleDateString(
-//   "en-US",
-//   {
-//     weekday: "long",
-//   }
-// );
-
-// var dayFive = new Date();
-// dayFive.setDate(today.getDate() + 4);
-// document.querySelector("#day-five").innerText = dayFive.toLocaleDateString(
-//   "en-US",
-//   {
-//     weekday: "long",
-//   }
-// );
-
 if (today.toLocaleDateString("en-US", {weekday: "long"}) === "Friday") {
-  document.querySelector("#upcoming-event-alert").innerHTML =
-    "&#129374; Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-  document.querySelector("#upcoming-event-alert")?.classList?.remove("hidden");
+  const currentPageCityID = document
+    .getElementById("individual-town-page")
+    .getAttribute("data-city-id");
+
+  if (currentPageCityID === "5604473") {
+    document.querySelector("#upcoming-event-alert").innerHTML =
+      "&#129374; Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    document
+      .querySelector("#upcoming-event-alert")
+      ?.classList?.remove("hidden");
+  }
 }
 
 WebFont.load({
