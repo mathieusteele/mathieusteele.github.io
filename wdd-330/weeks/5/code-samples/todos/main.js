f8b12d615ca7359676754fd1e06c3467655234a8
Mathieu Steele <matt@steeleagency.com>
2021-05-22 22:48:34 -0400

    initial commit for todos

---
+++ wdd-330/weeks/5/code-samples/todos/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const newTodoForm = document.getElementById("newTodoForm");
+
+newTodoForm.addEventListener("submit", (event) => {
+  // Intercept form submission
+
+  // Serialize the input
+
+  // Read To Dos from LocalStorage
+
+  // Create a new To Do
+
+  // Save To Dos to LocalStorage
+
+  // Update To Do List
+
+  // Reset Form
+
+  event.preventDefault();
+});
