1335c54cb1c616703731e4f53efbd2cd611a1135
Mathieu Steele <matt@steeleagency.com>
2021-05-25 07:40:57 -0400

    add filters and styles

--- wdd-330/weeks/5/code-samples/todos/js/main.js
+++ wdd-330/weeks/5/code-samples/todos/js/main.js
@@ -8,14 +8,16 @@ class Todo {
   }
 }
 
-const currentTodos = localStorage.getItem("todos")
-  ? JSON.parse(localStorage.getItem("todos"))
-  : [];
-
 function getTodos() {
   return localStorage.getItem("todos")
     ? JSON.parse(localStorage.getItem("todos"))
     : [];
+}
+
+function getTodoFilter() {
+  return localStorage.getItem("todo-filter")
+    ? localStorage.getItem("todo-filter")
+    : "all";
 }
 
 const toggleCompletion = (todoId) => {
@@ -37,7 +39,12 @@ const deleteTodo = (todoId) => {
 
   todos.splice(todos.indexOf(todoToRemove), 1);
   localStorage.setItem("todos", JSON.stringify(todos));
-  renderTodos(todos);
+  renderTodos();
+};
+
+const setFilter = (newValue) => {
+  localStorage.setItem("todo-filter", newValue);
+  renderTodos();
 };
 
 const renderOneTodo = (todo) => {
@@ -47,27 +54,74 @@ const renderOneTodo = (todo) => {
     todo.id
   });" ${todo.completed ? "checked" : ""}/> ${
     todo.content
-  } <button type="button" onClick="editTodo(${
+  } <button type="button" onClick="deleteTodo(${
     todo.id
-  })">Edit</button> <button type="button" onClick="deleteTodo(${
+  })">Delete</button> <button type="button" onClick="editTodo(${
     todo.id
-  })">Delete</button>`;
+  })">Edit</button> `;
 
   return listItem;
 };
 
-const renderTodos = (todos) => {
+const renderTodos = () => {
+  let todos = getTodos();
+  const filter = getTodoFilter();
   const container = document.getElementById("todos");
 
   container.innerHTML = "";
 
+  if (filter == "completed") {
+    todos = todos.filter((todo) => todo.completed);
+    // .map((todo) => {
+    //   container.appendChild(renderOneTodo(todo));
+    // });
+  } else if (filter == "incomplete") {
+    todos = todos.filter((todo) => !todo.completed);
+    // .map((todo) => {
+    //   container.appendChild(renderOneTodo(todo));
+    // });
+  }
+
   todos.map((todo) => {
     container.appendChild(renderOneTodo(todo));
   });
+
+  let controls = document.getElementById("controls");
+
+  let allButton = document.createElement("button");
+  allButton.innerText = "All";
+  allButton.setAttribute("type", "button");
+  allButton.onclick = () => setFilter("all");
+  if (filter == "all") {
+    allButton.setAttribute("disabled", true);
+  }
+
+  let incompleteButton = document.createElement("button");
+  incompleteButton.innerText = "Incomplete";
+  incompleteButton.setAttribute("type", "button");
+  incompleteButton.onclick = () => setFilter("incomplete");
+  if (filter == "incomplete") {
+    incompleteButton.setAttribute("disabled", true);
+  }
+
+  let completedButton = document.createElement("button");
+  completedButton.innerText = "Completed";
+  completedButton.setAttribute("type", "button");
+  completedButton.onclick = () => setFilter("completed");
+  if (filter == "completed") {
+    completedButton.setAttribute("disabled", true);
+  }
+
+  controls.innerHTML = "Filters: ";
+  controls.appendChild(allButton);
+  controls.appendChild(incompleteButton);
+  controls.appendChild(completedButton);
+
+  // controls.innerHTML = `<button type="button" onClick="setFilter('all')">All</button> <button type="button" onClick="setFilter('incomplete')">Incomplete</button> <button type="button"  onClick="setFilter('completed')">Completed</button>`;
 };
 
 // Initial render on page load
-renderTodos(currentTodos);
+renderTodos();
 
 const newTodoForm = document.getElementById("newTodoForm");
 
@@ -80,7 +134,7 @@ newTodoForm.addEventListener("submit", (event) => {
 
   localStorage.setItem("todos", JSON.stringify(updatedTodos));
 
-  renderTodos(updatedTodos);
+  renderTodos();
 
   newTodoForm.reset();
 
