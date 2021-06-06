010bfaac41bf062239e9787e990617584da9600f
Mathieu Steele <matt@steeleagency.com>
2021-06-05 22:03:24 -0400

    add collatz conjecture work

---
+++ wdd-330/weeks/7/code-samples/recursive-functions/collatz.js
@@ -0,0 +1,23 @@
+function collatz(n, sequence = [n]) {
+  if (n === 1) {
+    return `Sequence took ${sequence.length} steps. It was ${sequence}`;
+  }
+  if (n % 2 === 0) {
+    n = n / 2;
+  } else {
+    n = 3 * n + 1;
+  }
+  return collatz(n, [...sequence, n]);
+}
+
+const collatzForm = document.getElementById("collatzForm");
+
+collatzForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let number = parseInt(document.getElementById("number").value);
+
+  let result = collatz(number);
+
+  document.getElementById("result").innerText = result;
+});
